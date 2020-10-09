e0ce1d6da7e5d96e00d9f5841b42ac38c45cf9d5
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-10-09 14:45:39 -0600

    Redid it so listOfDays does not use let

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import DayListItem from "components/DayListItem.jsx";
 
 export default function DayList(props) {
-  let listOfDays = [];
-
-  if (props.days !== []){
-  listOfDays = props.days.map((day) => (
-    <DayListItem
-      key={"day_"+day.id}
-      name={day.name}
-      spots={day.spots}
-      selected={day.name === props.day}
-      setDay={props.setDay}
-    />
-  ));
-  }
+  const listOfDays = (props.days !== []) ?
+    listOfDays = props.days.map((day) => (
+      <DayListItem
+        key={"day_" + day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    ))
+    : [];
 
   return <ul>{listOfDays}</ul>;
 }
