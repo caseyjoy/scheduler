89652328b7ebc876b10f47199d9bead9719df4b1
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-10-08 22:34:29 -0600

    Added appointment editing

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -15,7 +15,7 @@ export default function Show(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <img
-            onClick={props.onEdit}
+            onClick={props.onEditClick}
             className="appointment__actions-button"
             src="images/edit.png"
             alt="Edit"
--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -12,6 +12,7 @@ import Confirm from "components/Appointment/Confirm.jsx";
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const EDIT = "EDIT";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
@@ -62,53 +63,66 @@ export default function Appointment(props) {
   }
 
   let element;
-  if (mode === CREATE) {
-    element = (
-      <Form interviewers={props.interviewers} onSave={save} onCancel={back} />
-    );
-  } else if (mode === SAVING) {
-    element = <Status message="Saving interview..." />;
-  } else if (mode === CONFIRM) {
-    element = (
-      <Confirm
-        onConfirm={() => {
-          onDelete();
-        }}
-        onCancel={back}
-        message="Really delete?"
-      />
-    );
-  } else if (mode === DELETING) {
-    element = <Status message="Deleting interview..." />;
-    /*     if (props.interview.student == "" || props.interview.interviewer == null)
-    transition(SHOW, REPLACE) */
-  } else if (mode === SHOW) {
-    // TODO: Add check for no results
-    element = (
-      <Show
-        onDeleteIconClick={() => {
-          onDeleteIconClick();
-        }}
-        student={props.interview ? props.interview.student : ""}
-        interviewer={
-          props.interview
-            ? props.interviewers.filter(
+  switch (mode) {
+    case EDIT:
+      element = (
+        <Form name={props.interview.student} interviewer={props.interview.interviewer} interviewers={props.interviewers} onSave={save} onCancel={back} />
+      );
+    break;
+
+    case CREATE:
+      element = (
+        <Form interviewers={props.interviewers} onSave={save} onCancel={back} />
+      );
+      break;
+
+    case SAVING:
+      element = <Status message="Saving interview..." />;
+      break;
+    case CONFIRM:
+      element = (
+        <Confirm
+          onConfirm={() => {
+            onDelete();
+          }}
+          onCancel={back}
+          message="Really delete?"
+        />
+      );
+      break;
+
+    case DELETING:
+      element = <Status message="Deleting interview..." />;
+      break;
+
+    case SHOW:
+      element = (
+        <Show
+          onEditClick={()=>{ transition(EDIT, REPLACE); }}
+          onDeleteIconClick={() => {
+            onDeleteIconClick();
+          }}
+          student={props.interview ? props.interview.student : ""}
+          interviewer={
+            props.interview
+              ? props.interviewers.filter(
                 (i) => i.id === props.interview.interviewer
               )[0]
-            : null
-        }
-      />
-    );
-  }
-  // interviewer={props.interview ? props.interviewers.filter(i => i === props.interview.interviewer.id) : null } />
-  else {
-    element = (
-      <Empty
-        onAdd={() => {
-          transition(CREATE);
-        }}
-      />
-    );
+              : null
+          }
+        />
+      );
+      break;
+
+    default:
+      element = (
+        <Empty
+          onAdd={() => {
+            transition(CREATE);
+          }}
+        />
+      );
+      break;
   }
 
   return (
