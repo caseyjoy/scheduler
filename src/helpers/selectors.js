ad47ea2548a7a358dbfabfad6cf08735d2ec7d6d
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-10-09 13:43:05 -0600

    Cleaned up and commented selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,18 +1,26 @@
+// Takes a list of days, and returns the one with the right name
 function getDayWithName(days, day){
-  return days.filter(checkday => checkday.name === day);
+  const search = days.filter(checkday => checkday.name === day);
+  if (search.length > 0){
+    return search[0]
+  }
+    else {
+      return false;
+    }
 }
 
 function getAppointmentsForDay(state, newDay) {
   const search = getDayWithName(state.days, newDay);
 
-  if (search.length > 0) {
-    const appList = search[0].appointments;
-    return appList.reduce(function (result, a) {
+  if (search) {
+    // looks through each appointment, makes a list of the ones on the right day, then returns them
+    return search.appointments.reduce(function (result, a) {
       if (a !== []) result.push(state.appointments[a]);
       return result;
     }, []);
     
   } else {
+    // if the search for the day failed, just return an empty list
     return [];
   }
 }
@@ -20,19 +28,17 @@ function getAppointmentsForDay(state, newDay) {
 function getInterviewersForDay(state, newDay){
   const search = getDayWithName(state.days, newDay);
 
-  if (search.length > 0) {
-    const appList = search[0].interviewers;
-    return appList.reduce(function (result, a) {
+  if (search) {
+    // looks through each interviewer, makes a list of the ones on the right day, then returns them
+    return search.interviewers.reduce(function (result, a) {
       if (a !== []) result.push(state.interviewers[a]);
       return result;
     }, []);
     
   } else {
+     // if the search for the day failed, just return an empty list
     return [];
   }
 }
-
-
-
 
 export { getAppointmentsForDay, getInterviewersForDay, getDayWithName };
