c6543f4a4de0617bb5606bbff105cc78a8a4d25f
Casey Joy <67843601+caseyjoy@users.noreply.github.com>
2020-10-09 22:15:54 -0600

    Changed DayList to use a spread prop, changed List to not always include {...item}

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -25,7 +25,7 @@ import DayListItem from "components/DayListItem.jsx";
 export default function DayList(props) {
   return (
     <ul>
-      <List {...props} type="DayList" listItem={DayListItem} compare="name" />
+      <List {...props} spread type="DayList" listItem={DayListItem} compare="name" />
     </ul>
   );
 }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,21 +5,28 @@ export default function List(props) {
   const ListItem = props.listItem;
 
   if (props.items) {
+    // create an array of ListItem, using the props we added.
+    // props.compare is the property we compare to the current value to see if it's actually the right thing  
+    // if props.spread is true, spread the properties in item into the ListItem
+
+    // TODO: figure out why DayList wants spread enabled - what in item is it using?
+
     const listOfItems =
       props.items !== []
         ? props.items.map((item, index) => (
             <ListItem
-              {...(props.spread && item)}
               key={props.type + item.id + index}
-              {...item}
               selected={item[props.compare] === props.value}
               onChange={props.onChange}
+              {...(props.spread && item)}
             />
           ))
         : [];
 
+    // returned in a fragment because the component calling this should always put them in something else
     return <>{listOfItems}</>;
   } else {
+    // had to return something to get it to not crash when there were no props
     return <></>
   }
 }
